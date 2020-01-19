e0385d6df8081e39579fbf617a8e1b9e243c3e59
khosro nazari <khosronz.com@gmail.com>
2020-01-19 10:01:15 +0330

    Some Translation

--- resources/lang/fa/passwords.php
+++ resources/lang/fa/passwords.php
@@ -3,5 +3,8 @@
 return [
     'sent'	        => 'لینک رمز عبور شما ارسال شده است!',
     'user'			=> 'هیچ کاربر فعالی با این آدرس ایمیل یافت نشد.',
+    'token'         => 'The password reset token you have provided is invalid',
+    'password'      => 'Passwords must be at least six characters and match the confirmation.',
+    'reset'         => 'Your password has been reset'
 ];
 
